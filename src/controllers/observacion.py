7a4f04f4377eaa14a55a1272097e575efd335e88
zoe <zoe.zapettini210429@alumnos.info.unlp.edu.ar>
2025-11-17 20:57:59 -0300

    obtencion de observaciones para determinada etapa

--- src/controllers/observacion.py
+++ src/controllers/observacion.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from classes.access import AccessAPI
 from activities.completar_actividad_siguiente import cargar_observacion as bonita_cargar_observacion
+from activities.completar_actividad_siguiente import obtener_observaciones_por_etapa
+import json
 from services.etapa_service import obtener_etapa_por_id
 observacion_bp = Blueprint('observacion', __name__)
 
@@ -15,8 +17,25 @@ def cargar_observacion():
         bonita_cargar_observacion(case_id, etapa.etapa_cloud_id, observacion)
 
         flash("Observación cargada correctamente", "success")
-        return redirect(url_for("formulario.index"))
+        return redirect(url_for("formulario.ver_proyectos_completados", case_id=case_id))
 
     return render_template("cargar_observacion.html",
                            etapa_id=etapa_id,
                            case_id=case_id)
+
+@observacion_bp.route('/ver_observaciones/', methods=['GET'])
+def ver_observaciones_por_etapa():
+    etapa_id = request.args.get("etapa_id") or request.form.get("etapa_id")
+    case_id = request.args.get("case_id") or request.form.get("case_id")
+    etapa = obtener_etapa_por_id(etapa_id)
+    response = obtener_observaciones_por_etapa(case_id, etapa.etapa_cloud_id)
+
+    data = response.get_json()
+    observaciones = data.get("observaciones", [])
+
+    return render_template(
+        "ver_observaciones.html",
+        etapa=etapa,
+        observaciones=observaciones,
+        case_id=case_id
+    )
